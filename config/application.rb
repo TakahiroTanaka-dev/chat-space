3a6d231e53f291c3565ddae70d0df6f7a50989b3
TakahiroTanaka-dev <takahiro.forwork1130@gmail.com>
2020-06-29 17:15:27 +0900

    Fixed spelling mistake

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,6 @@ module ChatSpace
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.i18n.default_locale = :ja
-    condig.time_zone = 'Tokyo'
+    config.time_zone = 'Tokyo'
   end
 end

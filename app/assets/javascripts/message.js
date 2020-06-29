97b8bd4e3714f99f105bfb22953f248907afb5df
TakahiroTanaka-dev <takahiro.forwork1130@gmail.com>
2020-06-29 12:26:42 +0900

    Set respond_to and create jbuilder

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,16 @@
 $(function(){
   $('.main-chat__bottom__form').on('submit', function(e){
     e.preventDefault();
-    console.log('aaa');
+    let formData =new FormData(this)
+    console.log(formData);
+    let url =$(this).attr('action');
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
   })
 })
